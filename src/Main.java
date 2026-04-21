f6bf09dc29709c5e6ae90f5ccfcc71a17c281fda
xocky2 <giovanniphilotech@gmail.com>
2026-04-21 17:02:04 -0300

    add Mentoria and Curso

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso .Net");
+        curso2.setDescricao("Descrição curso .Net");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+
+
+    }
+}
